053936cce59aa51eb93f66fb51a49dc75b3b09e1
johnhungerford <hungerfordjustice@gmail.com>
2019-05-31 09:31:53 -0400

    updating mysql files for security

---
+++ mysql/add_rows.sql
@@ -0,0 +1,10 @@
+INSERT INTO configs (id, config) VALUES (
+    'session',
+    '{ \"secret\": \"S3oqmn6dChVCqsAouT1n2OsZbZOJ8OAPl8CRE75oko198XG7WHSIGKoWnqVYDWROfxZ\", \"resave\": true, \"saveUninitialized\": true, \"maxAge\": \"7000000\" }'
+);
+
+INSERT INTO configs (id, config) VALUES (
+    'jwt',
+    '{ "secret": "as8d7fyeiuthw3jgb4356dkfjv336bsdntl6kge9ghub7fkbngweoruh9qfnlerfvnsi" }'
+);
+
